e0c60161baf87c1b9f2f6f363e00823d1f46d29c
TheArtofSatoshi <theartofsatoshi@gmail.com>
2026-02-02 23:10:52 -0600

    Add build timestamp and clarify Docker build/port documentation
    - Add build timestamp comment (2026-02-02) to Dockerfile header
    - Add --no-cache flag recommendation to docker-compose build step
    - Update port 8000 description from "RPC interface" to "REST API (HTTP)"
    - Add port 8001 documentation for "Admin RPC (localhost only)"
    - Improve quick start instructions with build context guidance

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ setuptools.setup(
     author='Neil Booth',
     author_email='kyuupichan@gmail.com',
     license='MIT Licence',
-    url='https://github.com/Radiant-Core/ElectrumX',
+    url='https://github.com/Radiant-Core/RXinDexer',
     long_description='Server implementation for the Electrum protocol',
-    download_url=('https://github.com/Radiant-Core/ElectrumX/archive/'
+    download_url=('https://github.com/Radiant-Core/RXinDexer/archive/'
                   f'{version}.tar.gz'),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
